a4b08b80766f53996e39b8336bf568818765ae94
PanwalaKhadija20 <panwalakhadija@gmail.com>
2026-07-30 13:14:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="hero">
+        <img src={heroImg} alt="Hero" className="hero-img" />
+
+        <h1>Welcome to React</h1>
+
+        <p>Build amazing websites with React.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">MyWebsite</div>
+
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <button className="login-btn">Login</button>
+    </nav>
+  );
+}
+
+export default Navbar;
